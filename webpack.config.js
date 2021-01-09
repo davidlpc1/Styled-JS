3ac9730f2596ff45d775339beb974dd174d5748d
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-09 20:29:23 -0300

    Updatin package

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require( 'path' );module.exports = {
     context: __dirname,
     entry: './src/index.js',
     output: {
-        path: path.resolve( __dirname, 'dist' ),
-        filename: 'main.js',
+        path: path.resolve( __dirname, 'src' ),
+        filename: 'bundle.js',
     },
     module: {
         rules: [
